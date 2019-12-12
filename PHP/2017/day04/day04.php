7e2d12f43e4b7cd9f40a6f9608612e112eca0ce3
enochc <enochc@kengarff.com>
2019-12-12 09:32:54 -0700

    adding the completed day04

--- PHP/2017/day04/day04.php
+++ PHP/2017/day04/day04.php
@@ -1,16 +1,38 @@
 <?php
 
 
-
-$phrases = file_get_contents('./input/input.txt');
-$input = explode("\n", $phrases);
+$phrases = file_get_contents('input/input.txt');
+$input = explode(PHP_EOL, $phrases);
 $count = 0;
-foreach($input as $pwd){
-    $words = explode(' ', $pwd);
-    $unique = array_unique($words);
-    if(count($words) === count($unique)){
-        echo $pwd . "<br>";
-        $count += 1;
+$part = 2;
+
+if($part == 1)
+{
+    foreach($input as $pwd){
+        $pwd = trim($pwd);
+        $words = explode(' ', $pwd);
+        $unique = array_unique($words);
+        if($words == $unique){
+            $count += 1;
+        }
+    }
+}
+elseif($part == 2)
+{
+    foreach($input as &$pwd)
+    {
+        $pwd = trim($pwd);
+        $words = explode(' ', $pwd);
+        foreach($words as &$word)
+        {
+            $str = str_split($word);
+            sort($str);
+            $word = implode('', $str);
+        }
+        $unique = array_unique($words);
+        if($words == $unique){
+            $count += 1;
+        }
     }
 }
 
